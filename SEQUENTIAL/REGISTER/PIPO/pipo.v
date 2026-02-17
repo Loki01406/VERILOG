7aa7f11376604e9e0f3238286678df8afa17cb13
LOKESH A S <as.lokesh1462004@gmail.com>
2026-02-17 15:51:35 +0530

    Create pipo.v

---
+++ SEQUENTIAL/REGISTER/PIPO/pipo.v
@@ -0,0 +1,13 @@
+module pipo(clk,reset,d,q);
+input clk,reset;
+input[3:0]d;
+output[3:0]q;
+reg[3:0]q;
+always@(posedge clk or posedge reset)
+begin
+if(reset)
+q<=4'b0000;
+else
+q<=d;
+end
+endmodule

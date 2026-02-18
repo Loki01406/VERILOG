7ef7ebd3823855ec4f0fadeaccb4f4d1a87d8609
LOKESH A S <as.lokesh1462004@gmail.com>
2026-02-18 15:33:40 +0530

    Create bidirectiona_reg.v

---
+++ SEQUENTIAL/REGISTER/BIDIRECTIONAL REGISTER/bidirectiona_reg.v
@@ -0,0 +1,33 @@
+module tb_bidirectional;
+reg clk;
+reg rst;
+reg dir;
+reg din;
+wire [3:0] q;
+bidirectional uut (
+    .clk(clk),
+    .rst(rst),
+    .dir(dir),
+    .din(din),
+    .q(q)
+);
+always #5 clk = ~clk;
+initial begin
+    clk = 0;
+    rst = 1;
+    dir = 0;
+    din = 0;
+    #10 rst = 0;
+    dir = 0; din  = 1; #10;
+    din = 0; #10;
+    din = 1; #10;
+    din = 1; #10;
+    dir = 1; din = 0; #10;
+    din = 1; #10;
+    din = 0; #10;
+    din = 1; #10;
+
+    #20 $finish;
+end
+
+endmodule

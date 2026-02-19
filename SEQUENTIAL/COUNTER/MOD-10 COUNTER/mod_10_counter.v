01f13450250a9fcb5caeb2fee85af6551bad7e5d
LOKESH A S <as.lokesh1462004@gmail.com>
2026-02-19 12:32:25 +0530

    Create mod_10_counter.v

---
+++ SEQUENTIAL/COUNTER/MOD-10 COUNTER/mod_10_counter.v
@@ -0,0 +1,11 @@
+module mod10_counter(input clk,input rst,output reg [3:0] q);
+always @(posedge clk or posedge rst)
+begin
+    if (rst)
+        q <= 4'b0000;
+    else if (q == 4'd9)
+        q <= 4'b0000;
+    else
+        q <= q + 1;
+end
+endmodule

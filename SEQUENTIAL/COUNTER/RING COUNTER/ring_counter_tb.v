4d867dad2aae8bf14b5cbc4712a11254b3f134e6
LOKESH A S <as.lokesh1462004@gmail.com>
2026-02-19 12:37:43 +0530

    Create ring_counter_tb.v

---
+++ SEQUENTIAL/COUNTER/RING COUNTER/ring_counter_tb.v
@@ -0,0 +1,16 @@
+module tb_ring_counter;
+reg clk;
+reg rst;
+wire [3:0] q;
+ring_counter uut(.clk(clk),.rst(rst),.q(q));
+always #5 clk = ~clk;
+initial begin
+    $dumpfile("ring_counter.vcd");
+    $dumpvars(0, tb_ring_counter);
+    $monitor("Time=%0t clk=%b rst=%b q=%b", $time, clk, rst, q);
+    clk = 0;
+    rst = 1;
+    #10 rst = 0;
+    #100 $finish;
+end
+endmodule

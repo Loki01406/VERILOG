2ccdece03f4232e9325cf65a8fe8f62852c7699e
LOKESH A S <as.lokesh1462004@gmail.com>
2026-02-19 12:23:13 +0530

    Create sync_up_counter_tb.v

---
+++ SEQUENTIAL/COUNTER/UP COUNTER/sync_up_counter_tb.v
@@ -0,0 +1,17 @@
+module tb_sync_up_counter;
+reg clk;
+reg rst;
+wire [3:0] q;
+sync_up_counter uut(.clk(clk),.rst(rst),.q(q));
+always #5 clk = ~clk;
+initial
+begin
+    $dumpfile("sync_up_counter.vcd");
+    $dumpvars(0, tb_sync_up_counter);
+    $monitor("Time=%0t clk=%b rst=%b q=%b", $time, clk, rst, q);
+    clk = 0;
+    rst = 1;
+    #10 rst = 0;
+    #100 $finish;
+end
+endmodule

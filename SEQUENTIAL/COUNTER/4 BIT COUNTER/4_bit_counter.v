bdea95168a7ab4b62eca9eedf9771218d8a08f34
LOKESH A S <as.lokesh1462004@gmail.com>
2026-02-19 12:19:06 +0530

    Create 4_bit_counter.v

---
+++ SEQUENTIAL/COUNTER/4 BIT COUNTER/4_bit_counter.v
@@ -0,0 +1,9 @@
+module counter_4bit_sync(input clk, input rst,output reg [3:0] q);
+always @(posedge clk or posedge rst)
+begin
+    if (rst)
+        q <= 4'b0000;
+    else
+        q <= q + 1;
+end
+endmodule

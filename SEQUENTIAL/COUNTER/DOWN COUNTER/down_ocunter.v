e283b3596aa0b4d69c912260fbb8a400cbf75a37
LOKESH A S <as.lokesh1462004@gmail.com>
2026-02-19 12:24:20 +0530

    Create down_ocunter.v

---
+++ SEQUENTIAL/COUNTER/DOWN COUNTER/down_ocunter.v
@@ -0,0 +1,9 @@
+module sync_down_counter(  input clk,input rst,output reg [3:0] q);
+always @(posedge clk or posedge rst)
+begin
+    if (rst)
+        q <= 4'b1111;
+    else
+        q <= q - 1;
+end
+endmodule

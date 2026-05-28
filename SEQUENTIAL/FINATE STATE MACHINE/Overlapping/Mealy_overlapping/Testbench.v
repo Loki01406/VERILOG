75cf2eac95fab7e2690a21e4c4f41311f2c368fa
LOKESH A S <as.lokesh1462004@gmail.com>
2026-05-28 10:01:31 +0530

    Create Testbench.v

---
+++ SEQUENTIAL/FINATE STATE MACHINE/Overlapping/Mealy_overlapping/Testbench.v
@@ -0,0 +1,22 @@
+module tb_mealy_overlapping;
+    reg clk, reset, x;
+    wire y;
+    mealy_overlapping dut(clk, reset, x, y);
+    always#5 clk = ~clk;
+    initial begin
+        $dumpfile("dump.vcd");
+        $dumpvars(0, tb_mealy_overlapping);
+        clk = 0;
+        reset = 1;
+        x = 0;
+        #10 reset = 0;
+        #10 x = 1;
+        #10 x = 1;
+        #10 x = 0;
+        #10 x = 1;
+        #10 x = 1;
+        #10 x = 0;
+        #10 x = 1;
+        #50 $finish;
+    end
+endmodule

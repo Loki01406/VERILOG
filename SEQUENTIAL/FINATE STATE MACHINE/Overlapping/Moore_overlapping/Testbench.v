515225814c5f8a94b67622fcc7859ff95661e8d6
LOKESH A S <as.lokesh1462004@gmail.com>
2026-05-28 10:12:16 +0530

    Create Testbench.v

---
+++ SEQUENTIAL/FINATE STATE MACHINE/Overlapping/Moore_overlapping/Testbench.v
@@ -0,0 +1,22 @@
+module tb_moore_overlapping;
+    reg clk, reset, x;
+    wire y;
+    moore_overlapping dut(clk, reset, x, y);
+    always#5 clk = ~clk;
+    initial begin
+        $dumpfile("dump.vcd");
+        $dumpvars(0, tb_moore_overlapping);
+        clk = 0;
+        reset = 1;
+        x = 0;
+        #10 reset = 0;
+        #10 x = 1;
+        #10 x = 1;
+        #10 x = 0;
+        #10 x = 1;
+        #10 x = 1;
+        #10 x = 0;
+        #10 x = 1;
+        #50 $finish;
+    end
+endmodule

07c849703cd3e3ba2da712415435391301a2451c
LOKESH A S <as.lokesh1462004@gmail.com>
2026-05-28 10:21:38 +0530

    Create Design.v

---
+++ SEQUENTIAL/FINATE STATE MACHINE/Non-Overlapping/Moore_Non-overlaping/Design.v
@@ -0,0 +1,32 @@
+module moore_nonoverlapping(
+    input clk,
+    input reset,
+    input x,
+    output reg y
+);
+    parameter S0 = 3'b000,
+              S1 = 3'b001,
+              S2 = 3'b010,
+              S3 = 3'b011,
+              S4 = 3'b100;
+    reg[2:0] state,next_state;
+    always@(posedge clk or posedge reset) begin
+        if(reset)
+            state <= S0;
+        else
+            state <= next_state;
+    end
+    always@(*) begin
+        case(state)
+            S0:next_state = x?S1:S0;
+            S1:next_state = x?S2:S0;
+            S2:next_state = x?S2:S3;
+            S3:next_state = x?S4:S0;
+            S4:next_state = x?S0:S0;
+            default:next_state = S0;
+        endcase
+    end
+    always@(*) begin
+        y=(state == S4);
+    end
+endmodule
